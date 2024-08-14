d0f4b2c0257609af5510c2aee10721b4808e1591
ironshine <cjfgml2320@gmail.com>
2024-08-14 17:15:25 +0900

    init commit

---
+++ src/main/java/com/sparta/springmasterweek1/Springmasterweek1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springmasterweek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmasterweek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springmasterweek1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springmasterweek1/Springmasterweek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springmasterweek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmasterweek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
